cebe9a6f20f39e7272b1282b1c63641318a5c40e
mihdenis85 <mihdenis777@yandex.ru>
2020-02-26 17:59:38 +0500

    Add map_params

--- map_coder.py
+++ map_coder.py
@@ -2,6 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import QMainWindow
 
+from static_map import get_map
 from ui.mapcoderwindow import Ui_MapWindow
 
 
@@ -9,3 +10,20 @@ class MapCoder(QMainWindow, Ui_MapWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.search_button.clicked.connect(self.search)
+
+    def search(self):
+        '''
+        map_params = {
+            "l": "map",
+            "size": '450,450',
+            "pt": "~".join()
+            ])
+        }
+        '''
+        coords = self.search_edit.text()
+        try:
+            result_map = get_map(coords)
+            print(result_map)
+        except Exception:
+            print('error')
